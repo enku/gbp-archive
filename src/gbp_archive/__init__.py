1d791058e1319f0616e4040c545df6339ced5c90
Albert Hopkins <marduk@letterboxes.org>
2025-08-09 14:29:59 -0500

    don't call gbpcli's load_env
    gbpcli already does this.

--- src/gbp_archive/__init__.py
+++ src/gbp_archive/__init__.py
@@ -1,11 +1,5 @@
 """gbp-archive: dump and restore builds in Gentoo Build Publisher"""
 
-from gbpcli.utils import load_env
-
-# Load server-side environment variables before proceeding
-# pylint: disable=wrong-import-position
-load_env()
-
 from . import records, storage
 from .core import dump, restore, tabulate
 
